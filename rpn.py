26389b194c07a2ce3157c29bfdfb51c56f3a521f
jessepan <jessepan@umich.edu>
2017-03-17 19:14:19 -0400

    adv finish

--- rpn.py
+++ rpn.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import operator
+import readline
+from colored import fg, attr, fore, style
 
 OPERATIONS = {
     "+": operator.add,
@@ -10,25 +12,50 @@ OPERATIONS = {
     "^": operator.pow,
 }
 
+COLOR = {
+    "+": fore.YELLOW,
+    "-": fore.BLUE,
+    "*": fore.MAGENTA,
+    "/": fore.CYAN,
+    "^": fore.DARK_GRAY,
+}
+
 def calculate(arg):
     stack = []
-    for operand in arg.split():
-        try:
-            operand = float(operand)
-            stack.append(operand)
-        except:
-            arg2 = stack.pop()
-            arg1 = stack.pop()
-            operator_fn = OPERATIONS[operand]
-            result = operator_fn(arg1, arg2)
+    try:
+        for operand in arg.split():
+            try:
+                operand = float(operand)
+                stack.append(operand)
+            except:
+                arg2 = stack.pop()
+                arg1 = stack.pop()
+                operator_fn = OPERATIONS[operand]
+                result = operator_fn(arg1, arg2)
 
-            stack.append(result)
-    return stack.pop()
+                stack.append(result)
+        return stack.pop()
+    except:
+        print ("An error occured")
+        return None
 
 def main():
     while True:
-        rst = calculate(input("rpn calc> "))
-        print (rst)
+        equation = input("rpn calc> ")
+        rst = calculate(equation)
+        coloredEq = ""
+        for i in equation.split():
+            try:
+                num = float(i)
+                if num < 0:
+                    color = fore.RED
+                else:
+                    color = fore.WHITE
+                coloredEq += color + i + ' '
+            except:
+                coloredEq += COLOR[i] + i + ' '
+                
+        print (style.BOLD + coloredEq + fore.LIGHT_YELLOW + "= " + style.RESET + str(rst))
 
 if __name__ == '__main__': # Note: that's "underscore underscore n a m e ..."
     main()
